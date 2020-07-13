5cd431087db43fc1b451164d08e7c103ce3924ca
k018c1344 <k018c1344@it-neec.jp>
2020-07-13 14:03:30 +0900

    4.3.routing

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    list={'apple':'リンゴ','orange':'みかん','lemon':'レモン'}
-    return render_template('index.html',list=list)
+    return render_template('index.html',message="indexページ")
+
+@app.route('/hello/')
+def hello():
+    return render_template('hello.html',message="helloページ")
 
 if __name__=='__main__':
     app.debug = True
