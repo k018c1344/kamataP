9176e97140f09af741fc57e958daf492ba0f3b97
k018c1344 <k018c1344@it-neec.jp>
2020-07-13 14:12:58 +0900

    4.3.routing

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html',message="indexページ")
-
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html',message="helloページ")
+@app.route('/user/<username>/')
+def profile(username):
+    return render_template('index.html',message=username)
 
 if __name__=='__main__':
     app.debug = True
