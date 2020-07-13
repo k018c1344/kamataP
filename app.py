5a8a924dd33049d728f9718b9ef11bacac7910f9
k018c1344 <k018c1344@it-neec.jp>
2020-07-13 14:18:53 +0900

    4.4.routing(path)

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/user/<username>/')
-def profile(username):
-    return render_template('index.html',message=username)
+data=["AAA","BBB","CCC","DDD"]
+
+@app.route('/post/<int:post_id>/')
+def profile(post_id):
+    if post_id<len(data):
+        return render_template('index.html',message=data[post_id])
+    else:
+        return "0 ～ "+str(len(data)-1)+"の値を入力してください"
 
 if __name__=='__main__':
     app.debug = True
