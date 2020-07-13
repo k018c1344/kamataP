33dda1b55a3ea43543d811d9acf9ab04219f5511
k018c1344 <k018c1344@it-neec.jp>
2020-07-13 12:04:45 +0900

    3.3.template(dic)

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask render_template
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html',obj={"title":"タイトル2"})
 
 if __name__=='__main__':
     app.debug = True
