1e88f244f09e5eb8b7eb1487596b5a7a8cf26db2
k018c1344 <k018c1344@it-neec.jp>
2020-12-24 10:59:11 +0900

    commit db

--- home/KamataGallery.py
+++ home/KamataGallery.py
@@ -14,10 +14,10 @@ db_param={
 
 app = Flask(__name__)
 
-path='./files/'
+path='../static/files/'
 app.config['SECRET_KEY']=os.urandom(24)
 
-#----------------------------   画面表示
+#----------------------------   画面表示 ----------------------------
 
 @app.route('/')
 #初期画面遷移
@@ -59,26 +59,29 @@ def company():
 def enter():
     return render_template('enter.html')
 
+#----------------------- 各種処理 ---------------------
+
 @app.route('/login')
 #ログイン画面遷移
 def login():
-    if 'user_name' in session:
+    if 'user_ID' in session:
         #ログインした後ログアウトされていない場合
-        name=str(session['user_name'])
+        id=str(session['user_ID'])
+        name=str(session['user_ID'])
         return render_template('mypage.html',name=name)
     return render_template('message.html')
 
 @app.route('/login',methods=['POST'])
 #ログイン処理
 def login_send():
-    name=request.form.get('user_name')
+    id=request.form.get('user_ID')
     pw=request.form.get('password')
-    if name=="" or pw=="":
+    if id=="" or pw=="":
         return redirect('/login')
     conn=db.connect(**db_param)
     cur=conn.cursor()
-    stmt='SELECT * FROM sample1 WHERE name=%s AND password=%s'
-    cur.execute(stmt,(name,pw))
+    stmt='SELECT * FROM sample1 WHERE user_ID=%s AND password=%s'
+    cur.execute(stmt,(id,pw))
     rows=cur.fetchall()
     conn.commit()
     cur.close()
@@ -86,9 +89,10 @@ def login_send():
     if len(rows)==0:
         return redirect('/login')
     else:
-        if request.form.get('user_name'):
-            session['user_name']=request.form.get('user_name')
-        return render_template('mypage.html',name=session['user_name'])
+        if request.form.get('user_ID'):
+            session['user_ID']=request.form.get('user_ID')
+            FilePath=path+id
+        return render_template('mypage.html',user_ID=session['user_ID'],FilePath=FilePath)
 
 @app.route('/new')
 #新規登録画面遷移
@@ -98,49 +102,49 @@ def new():
 @app.route('/new',methods=['POST'])
 #新規登録処理
 def new_send():
-    name=request.form.get('user_name')
+    id=request.form.get('user_ID')
     pw=request.form.get('password')
-    if name=="" or pw=="":
+    if id=="" or pw=="":
         return redirect('/new')
-    if request.form.get('user_name'):
-        session['user_name']=request.form.get('user_name')
+    if request.form.get('user_ID'):
+        session['user_ID']=request.form.get('user_ID')
     conn=db.connect(**db_param)
     cur=conn.cursor()
-    stmt='SELECT * FROM sample1 WHERE name=%s'
-    cur.execute(stmt,(name,))
+    stmt='SELECT * FROM sample1 WHERE user_ID=%s'
+    cur.execute(stmt,(id,))
     rows=cur.fetchall()
     if len(rows)==0:
-        cur.execute('INSERT ignore INTO sample1(name,password) VALUES(%s,%s)',(name,pw))
-        FilePath=path+name
+        cur.execute('INSERT ignore INTO sample1(user_ID,password) VALUES(%s,%s)',(id,pw))
+        FilePath=path+id
         reference=os.path.exists(FilePath)
         if reference==False:
-                os.mkdir(FilePath)
+            os.mkdir(FilePath)
         conn.commit()
         cur.close()
         conn.close()
-        return render_template('mypage.html',name=name)
+        return render_template('mypage.html',user_ID=id)
     else:
         return redirect('/new')
 
 @app.route('/logout')
 #ログアウト処理
 def logout():
-    session.pop('user_name',None)
+    session.pop('user_ID',None)
     return redirect('/')
 
 @app.route('/send',methods=['POST'])
 #ファイル送信処理
 def upload():
-    if 'user_name' in session:
-        name=str(session['user_name'])
+    if 'user_ID' in session:
+        id=str(session['user_ID'])
     else:
         return redirect('/login')
 
     file=request.files['file']
-    FilePath=path+name
+    FilePath=path+id
     file.save(FilePath+'/'+file.filename)
     result=file.filename+'を送信しました'
-    return render_template('mypage.html',name=name,result=result)
+    return render_template('mypage.html',id=id,result=result)
 
 if __name__=='__main__':
     app.debug = True
