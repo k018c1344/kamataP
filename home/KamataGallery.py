5d783c8bf2e9ce328a199d49ef523b9190f54617
k018c1344 <k018c1344@it-neec.jp>
2020-12-25 10:54:15 +0900

    commit db1

--- home/KamataGallery.py
+++ home/KamataGallery.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 path='../static/files/'
 app.config['SECRET_KEY']=os.urandom(24)
 
-#----------------------------   画面表示 ----------------------------
+#---------------------------- 画面表示 ----------------------------
 
 @app.route('/')
 #初期画面遷移
@@ -66,22 +66,20 @@ def enter():
 def login():
     if 'user_ID' in session:
         #ログインした後ログアウトされていない場合
-        id=str(session['user_ID'])
-        name=str(session['user_ID'])
-        return render_template('mypage.html',name=name)
-    return render_template('message.html')
+        return render_template('mypage.html',user_ID=session['user_ID'])
+    return render_template('login.html')
 
 @app.route('/login',methods=['POST'])
 #ログイン処理
 def login_send():
-    id=request.form.get('user_ID')
+    u_id=request.form.get('user_ID')
     pw=request.form.get('password')
-    if id=="" or pw=="":
+    if u_id=="" or pw=="":
         return redirect('/login')
     conn=db.connect(**db_param)
     cur=conn.cursor()
     stmt='SELECT * FROM sample1 WHERE user_ID=%s AND password=%s'
-    cur.execute(stmt,(id,pw))
+    cur.execute(stmt,(u_id,pw))
     rows=cur.fetchall()
     conn.commit()
     cur.close()
@@ -97,32 +95,34 @@ def login_send():
 @app.route('/new')
 #新規登録画面遷移
 def new():
-    return render_template('message-new.html')
+    return render_template('register.html')
 
 @app.route('/new',methods=['POST'])
 #新規登録処理
 def new_send():
-    id=request.form.get('user_ID')
+    u_id=request.form.get('user_ID')
     pw=request.form.get('password')
-    if id=="" or pw=="":
+    if u_id=="" or pw=="":
         return redirect('/new')
     if request.form.get('user_ID'):
         session['user_ID']=request.form.get('user_ID')
     conn=db.connect(**db_param)
     cur=conn.cursor()
     stmt='SELECT * FROM sample1 WHERE user_ID=%s'
-    cur.execute(stmt,(id,))
+    cur.execute(stmt,(u_id,))
     rows=cur.fetchall()
     if len(rows)==0:
-        cur.execute('INSERT ignore INTO sample1(user_ID,password) VALUES(%s,%s)',(id,pw))
-        FilePath=path+id
-        reference=os.path.exists(FilePath)
-        if reference==False:
+        cur.execute('INSERT ignore INTO sample1(user_ID,password) VALUES(%s,%s)',(u_id,pw))
+        cur.execute('SELECT id FROM sample1 WHERE user_ID=%s',(u_id,))
+        id=cur.fetchall()
+        id=int(id[0][0])
+        FilePath='static/files/'+str(id)
+        if os.path.exists(FilePath)==False:
             os.mkdir(FilePath)
         conn.commit()
         cur.close()
         conn.close()
-        return render_template('mypage.html',user_ID=id)
+        return render_template('mypage.html',user_ID=u_id)
     else:
         return redirect('/new')
 
@@ -136,7 +136,7 @@ def logout():
 #ファイル送信処理
 def upload():
     if 'user_ID' in session:
-        id=str(session['user_ID'])
+        u_id=str(session['user_ID'])
     else:
         return redirect('/login')
 
@@ -144,7 +144,7 @@ def upload():
     FilePath=path+id
     file.save(FilePath+'/'+file.filename)
     result=file.filename+'を送信しました'
-    return render_template('mypage.html',id=id,result=result)
+    return render_template('mypage.html',user_ID=u_id,result=result)
 
 if __name__=='__main__':
     app.debug = True
